e3cf2f5b8974e3d37529a3493763ef989a639851
Ankit Gupta <ankitgupta@styledotme.com>
2024-05-28 17:15:49 +0530

    added prettier fixes

--- services/AR_Service.ts
+++ services/AR_Service.ts
@@ -1,57 +1,53 @@
-import { AR_Manager } from "@mirrar-sdm/minimal-ar-react-native-sdk"
-import type { ProductInput } from "@mirrar-sdm/minimal-ar-react-native-sdk"
+import { AR_Manager } from '@mirrar-sdm/minimal-ar-react-native-sdk';
+import type { ProductInput } from '@mirrar-sdm/minimal-ar-react-native-sdk';
 
 class AR_Service {
-    
-    arEngineSetupDone = false
-    currentMode = 'model'
+  arEngineSetupDone = false;
+  currentMode = 'model';
 
-    constructor() {
-        this.setupAR_Engine()
+  constructor() {
+    this.setupAR_Engine();
+  }
+
+  setupAR_Engine() {
+    console.log('Setting up AR Engine');
+    if (!this.arEngineSetupDone) {
+      AR_Manager.setupAR_Engine('face');
+      this.arEngineSetupDone = true;
     }
+  }
 
-    setupAR_Engine() {
-        console.log("Setting up AR Engine")
-        if(!this.arEngineSetupDone) {
-            AR_Manager.setupAR_Engine('face')
-            this.arEngineSetupDone = true
-        }
-    }
+  switchToModel(model: string) {
+    this.currentMode = 'model';
+    AR_Manager.switchToModelModeFromCamera(model);
+  }
 
-    switchToModel(model: string) {
-        this.currentMode = 'model'
-        AR_Manager.switchToModelModeFromCamera(model)
-    }
+  switchToCamera() {
+    this.currentMode = 'camera';
+    AR_Manager.switchToCameraModeFromModel();
+  }
 
-    switchToCamera() {
-        this.currentMode = 'camera'
-        AR_Manager.switchToCameraModeFromModel()
-    }
+  addProduct(product: ProductInput) {
+    AR_Manager.addProduct(product);
+  }
 
-    addProduct(product: ProductInput) {
-        AR_Manager.addProduct(product)
-    }
+  removeProduct(product: ProductInput) {
+    AR_Manager.removeProduct(product);
+  }
 
-    removeProduct(product: ProductInput) {
-        AR_Manager.removeProduct(product)
-    }
+  setupFaceTracking() {
+    AR_Manager.setupFaceTracking();
+  }
 
-    setupFaceTracking() {
-        AR_Manager.setupFaceTracking()
-    }
+  setupHandTracking() {
+    AR_Manager.setupHandTracking();
+  }
 
-    setupHandTracking() {
-        AR_Manager.setupHandTracking()
-    }
-
-    changeModelImage(model: string) {
-        this.switchToModel(model)
-    }
-
+  changeModelImage(model: string) {
+    this.switchToModel(model);
+  }
 }
 
-let arService = new AR_Service()
+let arService = new AR_Service();
 
-export {
-    arService as AR_Service
-}
+export { arService as AR_Service };
